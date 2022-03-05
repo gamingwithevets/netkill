a3fd6b2d40c065b217041d282d9c1fcf6e741e67
GamingWithEvets <88830673+gamingwithevets@users.noreply.github.com>
2022-03-05 21:24:49 +0700

    Create the CMD script

---
+++ netkill.bat
@@ -0,0 +1,19 @@
+@echo off
+
+set version=1.0
+
+title NetKill v.%version%
+
+cls
+echo NETKILL - NETSUPPORT SCHOOL KILLER v.%version%
+echo GitHub Repo: https://github.com/gamingwithevets/netkill
+echo.
+echo Killing NetSupport School...
+taskkill /f /im client32.exe >nul 2>&1
+if %errorlevel% == 0 (echo Killed client32.exe.) else (echo Can't kill client32.exe!)
+taskkill /f /im runplugin.exe >nul 2>&1
+if %errorlevel% == 0 (echo Killed runplugin.exe.) else (echo Can't kill runplugin.exe!)
+echo.
+echo All done! After closing this window, NetSupport School should be terminated.
+echo Press any key to exit.
+pause >nul
